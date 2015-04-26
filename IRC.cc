104d4991c16494cfb9f67fe2b65285b0e3ef741a
Kush Rustagi <krustagi@moore09.cs.purdue.edu>
2015-04-26 15:14:28 -0400

    Make IRCclient

--- IRC.cc
+++ IRC.cc
@@ -14,6 +14,10 @@
 #include <gtk/gtk.h>
 
 GtkListStore * list_rooms;
+GtkListStore * room_user_names;
+GtkWidget * room_entry;
+GtkWidget * create_room;
+GtkWidget * namelist;
 
 char * usern;
 char * passw;
@@ -36,6 +40,20 @@ void update_list_rooms() {
 	g_free (msg);
     }
 }
+
+void update_room_user_names() {
+    GtkTreeIter iter;
+    int i;
+
+    /* Add some messages to the window */
+    for (i = 0; i < 10; i++) {
+        gchar *msg = g_strdup_printf ("User %d", i);
+        gtk_list_store_append (GTK_LIST_STORE (room_user_names), &iter);
+        gtk_list_store_set (GTK_LIST_STORE (room_user_names), &iter, 0, msg,-1);
+		g_free (msg);
+    }
+}
+
 
 /* Create the list of "messages" */
 static GtkWidget *create_list( const char * titleColumn, GtkListStore *model )
@@ -110,7 +128,7 @@ static GtkWidget *create_text( const char * initialText )
 }
 
 
-char * host = "localhost";
+char * host;
 char * user;
 char * password;
 char * sport;
@@ -169,8 +187,7 @@ int open_client_socket(char * host, int port) {
 	return sock;
 }
 
-int sendCommand(char * host, int port, char * command, char * user,
-		char * password, char * args, char * response) {
+int sendCommand(char * host, int port, char * command, char * user, char * password, char * args, char * response) {
 	int sock = open_client_socket( host, port);
 
 	// Send command
@@ -203,13 +220,26 @@ void printUsage()
 
 void add_user() {
 	// Try first to add user in case it does not exist.
-	char response[ MAX_RESPONSE ];
+	char * response = (char *)g_malloc(100);
 	sendCommand(host, port, "ADD-USER", usern, passw, "", response);
 	
 	if (!strcmp(response,"OK\r\n")) {
 		printf("User %s added\n", user);
 	}
 }
+
+void fncreate_room() {
+	// Try first to add user in case it does not exist.
+	char * response = (char *)g_malloc(100);
+	sendCommand(host, port, "CREATE-ROOM", usern, passw, "", response);
+	
+	if (!strcmp(response,"OK\r\n")) {
+		printf("Room %s added\n", user);
+	}
+}
+
+
+
 
 
 
@@ -241,6 +271,20 @@ static void enter_callback( GtkWidget *widget,
   printf ("Entry contents: %s\n", entry_text);
   usern = (char *)entry_text;
 }
+
+char * sendrn;
+
+static void room_enter_callback( GtkWidget *widget,
+                            GtkWidget *entry )
+{
+  const gchar *entry_text;
+  entry_text = gtk_entry_get_text (GTK_ENTRY (entry));
+  printf ("Entry contents: %s\n", entry_text);
+  sendrn = (char *)entry_text;
+
+}
+
+
 static void pass_callback( GtkWidget *widget,
                             GtkWidget *entry )
 {
@@ -261,6 +305,16 @@ static void send_details( GtkWidget *widget, GtkWidget *w1)
   printf ("Entry contents: %s\n", entry_text2);
   passw = (char *)entry_text2;
   add_user();
+  
+}
+
+static void send_create_room(GtkWidget *widget, GtkWidget *w1)
+{
+  const gchar *entry_text;
+  entry_text = gtk_entry_get_text (GTK_ENTRY (w1));
+  printf ("Entry contents: %s\n", entry_text);
+  sendrn = (char *)entry_text;
+  fncreate_room();
   
 }
 
@@ -355,10 +409,13 @@ int main( int   argc,
     GtkWidget *messages;
     GtkWidget *myMessage;
 
+	host = "localhost";
+
 	//up = (userpass *)malloc(100*sizeof(userpass));
 
 	usern = (char *)g_malloc(100);
 	passw = (char *)g_malloc(100);
+	sendrn = (char *)g_malloc(100);
 
     gtk_init (&argc, &argv);
    
@@ -367,44 +424,67 @@ int main( int   argc,
     g_signal_connect (window, "destroy",
 	              G_CALLBACK (gtk_main_quit), NULL);
     gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-    gtk_widget_set_size_request (GTK_WIDGET (window), 600, 450);
+    gtk_widget_set_size_request (GTK_WIDGET (window), 800, 650);
 
     // Create a table to place the widgets. Use a 7x4 Grid (7 rows x 4 columns)
-    GtkWidget *table = gtk_table_new (7, 4, TRUE);
+    GtkWidget *table = gtk_table_new (10, 9, TRUE);
     gtk_container_add (GTK_CONTAINER (window), table);
-    gtk_table_set_row_spacings(GTK_TABLE (table), 5);
-    gtk_table_set_col_spacings(GTK_TABLE (table), 5);
+    gtk_table_set_row_spacings(GTK_TABLE (table), 7);
+    gtk_table_set_col_spacings(GTK_TABLE (table), 7);
     gtk_widget_show (table);
 
     // Add list of rooms. Use columns 0 to 4 (exclusive) and rows 0 to 4 (exclusive)
     list_rooms = gtk_list_store_new (1, G_TYPE_STRING);
     update_list_rooms();
     list = create_list ("Rooms", list_rooms);
-    gtk_table_attach_defaults (GTK_TABLE (table), list, 2, 4, 0, 2);
+    gtk_table_attach_defaults (GTK_TABLE (table), list, 0, 2, 0, 4);
     gtk_widget_show (list);
    
+	room_user_names = gtk_list_store_new (7, G_TYPE_STRING);
+    update_room_user_names();
+    namelist = create_list ("Users", room_user_names);
+    gtk_table_attach_defaults (GTK_TABLE (table), namelist, 0, 2, 6, 10);
+    gtk_widget_show (namelist);
+
+	room_entry = gtk_entry_new ();
+    gtk_entry_set_max_length (GTK_ENTRY (room_entry), 15);
+    g_signal_connect (room_entry, "activate", G_CALLBACK (room_enter_callback), room_entry);
+    gtk_entry_set_text (GTK_ENTRY (room_entry), "Room name");
+    //tmp_pos = GTK_ENTRY (room_name)->text_length;
+   // gtk_editable_insert_text (GTK_EDITABLE (entry), " world", -1, &tmp_pos);
+   // gtk_editable_select_region (GTK_EDITABLE (entry),
+	//		        0, GTK_ENTRY (entry)->text_length);
+   // gtk_box_pack_start (GTK_BOX (vbox), entry, TRUE, TRUE, 0);
+	gtk_table_attach_defaults(GTK_TABLE (table), room_entry, 0, 2, 4, 5); 
+    gtk_widget_show (room_entry);
+
+	// Add create_room button. Use columns 0 to 1 (exclusive) and rows 4 to 7 (exclusive)
+    GtkWidget *create_room = gtk_button_new_with_label ("Create Room");
+    gtk_table_attach_defaults(GTK_TABLE (table), create_room, 0, 2, 5, 6); 
+    gtk_widget_show (create_room);
+
+	g_signal_connect (create_room, "clicked", G_CALLBACK (send_create_room), room_entry);
+	
     // Add messages text. Use columns 0 to 4 (exclusive) and rows 4 to 7 (exclusive) 
     messages = create_text ("Peter: Hi how are you\nMary: I am fine, thanks and you?\nPeter: Fine thanks.\n");
-    gtk_table_attach_defaults (GTK_TABLE (table), messages, 0, 4, 2, 5);
+    gtk_table_attach_defaults (GTK_TABLE (table), messages, 2, 6, 0, 6);
     gtk_widget_show (messages);
     // Add messages text. Use columns 0 to 4 (exclusive) and rows 4 to 7 (exclusive) 
 
     myMessage = create_text ("I am fine, thanks and you?\n");
-    gtk_table_attach_defaults (GTK_TABLE (table), myMessage, 0, 4, 5, 7);
+    gtk_table_attach_defaults (GTK_TABLE (table), myMessage, 2, 6, 7, 9);
     gtk_widget_show (myMessage);
 
     // Add send button. Use columns 0 to 1 (exclusive) and rows 4 to 7 (exclusive)
     GtkWidget *send_button = gtk_button_new_with_label ("Send");
-    gtk_table_attach_defaults(GTK_TABLE (table), send_button, 0, 1, 7, 8); 
+    gtk_table_attach_defaults(GTK_TABLE (table), send_button, 2, 6, 9, 10); 
     gtk_widget_show (send_button);
 
-	 // Add ca button. Use columns 0 to 1 (exclusive) and rows 4 to 7 (exclusive)
-    GtkWidget *ca = gtk_button_new_with_label ("Create Account");
-    gtk_table_attach_defaults(GTK_TABLE (table), ca, 3, 4, 7, 8); 
+	GtkWidget * ca= gtk_button_new_with_label ("Create Account");
+    gtk_table_attach_defaults(GTK_TABLE (table), ca, 6, 9, 5, 6); 
     gtk_widget_show (ca);
 
 	g_signal_connect (ca, "clicked", G_CALLBACK (hello), NULL);
-	
     
     gtk_widget_show (table);
     gtk_widget_show (window);
