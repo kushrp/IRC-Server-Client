b762b75c6272b6a4095e481526d9b60f6723fc68
Kush Rustagi <krustagi@moore02.cs.purdue.edu>
2015-04-27 13:25:44 -0400

    Make IRCclient

--- IRCClient.c
+++ IRCClient.c
@@ -16,6 +16,8 @@
 #define MAX_MESSAGE_LEN 300
 #define MAX_RESPONSE (20 * 1024)
 
+char names[MAX_RESPONSE];
+char rnames[MAX_RESPONSE];
 int lastMessage = 0;
 
 char * user1;
@@ -252,9 +254,9 @@ void update_list_rooms() {
 	printf("1\n");
 	gtk_list_store_clear(GTK_LIST_STORE (list_rooms));
 	printf("2\n");
-	char response[MAX_RESPONSE];
-	sendCommand(host, port, "LIST-ROOMS", user, password, "", response);
-	char * token = strtok(response,"\r\n");
+	//char response[MAX_RESPONSE];
+	sendCommand(host, port, "LIST-ROOMS", user, password, "", rnames);
+	char * token = strtok(rnames,"\r\n");
     while(token != NULL) {
 		gchar *msg = g_strdup((gchar *)token);
         gtk_list_store_append (GTK_LIST_STORE (list_rooms), &iter);
@@ -277,10 +279,10 @@ void update_list_names() {
     else { */
 		//GtkTreeIter iter;
 		gtk_list_store_clear(GTK_LIST_STORE (list_names));
-		char response[MAX_RESPONSE];
-		sendCommand(host, port, "GET-USERS-IN-ROOM", user, password, strdup(roomname), response);
-		printf("Response Get-users-in-room: %s\n pls work",response);
-		char * token = strtok(response,"\r\n");
+		//char response[MAX_RESPONSE];
+		sendCommand(host, port, "GET-USERS-IN-ROOM", user, password, strdup(roomname), names);
+		printf("Response Get-users-in-room: %s\n pls work",names);
+		char * token = strtok(names,"\r\n");
     	while(token != NULL) {
 			printf("Token: %s\n",token);
 			gchar *msg = g_strdup((gchar *)token);
