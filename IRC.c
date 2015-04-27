2042b8a34adde3e106808e279f701493bcfb1209
Kush Rustagi <krustagi@moore01.cs.purdue.edu>
2015-04-27 03:33:01 -0400

    Make IRCclient

--- IRC.c
+++ IRC.c
@@ -56,8 +56,8 @@ void update_list_rooms() {
 	char response[MAX_RESPONSE] = "hi";
 	sendCommand(host, port, "LIST-ROOMS", "superman", "clarkkent", "", response);
 	printf("fff\n");
-	gtk_list_store_clear(GTK_LIST_STORE (list_rooms)); 
-	//printf("hi 1\n");
+	gtk_list_store_clear(list_rooms); 
+	printf("hi 1\n");
 	printf("1\n");
 	char * token = strtok(response,"\r\n");
 	printf("2\n");
