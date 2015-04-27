9aee72510d8b1eba8c2140c0e0a1af854d01ea24
Kush Rustagi <krustagi@moore02.cs.purdue.edu>
2015-04-27 18:31:37 -0400

    Make IRCclient

--- IRCClient.c
+++ IRCClient.c
@@ -390,8 +390,8 @@ time_handler(GtkWidget *widget)
   //gtk_widget_queue_draw(widget);
 
  // fprintf(stderr, "Hi\n");
-  //update_list_rooms();
-  getmsgs();
+  update_list_rooms();
+  //getmsgs();
   //update_list_names();
 
   return TRUE;
